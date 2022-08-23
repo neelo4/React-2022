cc5bb029ad71066cd43ff49487a808940cc0bed2
neelo4 <53837564+neelo4@users.noreply.github.com>
2022-08-23 18:04:16 +0530

    Feedback app

---
+++ feedback-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+import FeedbackList from "./components/FeedbackList";
+import Header from "./components/Header"
+import FeedbackData from "./data/FeedbackData"
+
+const App = () => {
+  const [feedback, setFeedback] = useState(FeedbackData);
+  return (
+    <>
+    <Header />
+   <div className="container">
+    <FeedbackList feedback = {feedback} />
+   </div>
+    </>
+  )
+}
+
+export default App
--- feedback-app/src/components/Feedbackitem.jsx
+++ feedback-app/src/components/Feedbackitem.jsx
@@ -1,16 +1,17 @@
-import {useState} from 'react'
-function Feedbackitem() {
-    const [rating, setRating] = useState(7);
-    const [text, setText] = useState('This is an example of feedback item');
-  
+
+
+const FeedbackItem = ({item}) => {
   return (
-    <div className="card">
-      <div className="num-display">{rating}</div>
-      <div className="text-display">{text}</div>
-     
-    </div>
+   <div className="card">
+    <div className="num-display">{item.rating}</div>
+    <div className="text-display">{item.text}</div>
+    <button>
+        
+    </button>
+   </div>
   )
 }
 
-export default Feedbackitem
+export default FeedbackItem
+
 
